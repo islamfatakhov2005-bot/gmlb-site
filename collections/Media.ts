d2fea46dabb169ac55f0f829c97c74da6a3c414c
Islam <islamfatakhov2005@gmail.com>
2026-03-05 20:09:45 +0300

    fix media upload path to public/media

--- collections/Media.ts
+++ collections/Media.ts
@@ -3,6 +3,7 @@ import type { CollectionConfig } from 'payload'
 export const Media: CollectionConfig = {
   slug: 'media',
   upload: {
+    staticDir: 'public/media',
     mimeTypes: ['image/*'],
     imageSizes: [
       {
